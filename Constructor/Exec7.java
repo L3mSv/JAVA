5b85e21770a11aa7cc71b12e5f596805f676abe3
Lucas <lucaslemes471@gmail.com>
2025-04-04 00:10:23 -0300

    A

--- Constructor/Exec7.java
+++ Constructor/Exec7.java
@@ -2,50 +2,38 @@
 Write a Java program to create a class called "ComplexInitializer" with a static block that initializes multiple static variables (x, y, z) using complex logic. Print the values of these variables in the main method.
 */
 
-<<<<<<< HEAD:Constructor/Exec7.java
 public class Exec7{
-    public static void main(String[] args){
-        Car car1 = new Car();
-        Car car2 = new Car("Porsche", "911 GT3 RS", 2024);
 
-        car1.GetCar();
-        car2.GetCar();
-=======
-public class Exec7 {
-    public static void main(String[] args) {
-        {
-            System.out.println("x: " + ComplexInitializer.x + "\ny: " + ComplexInitializer.y + "\nz: " + ComplexInitializer.z);
+        public static void main(String[] args) {
+
+            Car car1 = new Car("Toyota", "Corolla", 2021);
+
+            System.out.println("Car 1 Make: " + car1.make);
+            System.out.println("Car 1 Model: " + car1.model);
+            System.out.println("Car 1 Year: " + car1.year);
+    
+
+            Car car2 = new Car("", "", -1);
+
+            System.out.println("Car 2 Make: " + car2.make);
+            System.out.println("Car 2 Model: " + car2.model);
+            System.out.println("Car 2 Year: " + car2.year);
         }
->>>>>>> c21615ad385eea7496c6007ef32f7b0dad58704a:Exec7.java
-    }
 }
 
-class ComplexInitializer{
-    static int x;
-    static int y;
-    static int z;
 
-<<<<<<< HEAD:Constructor/Exec7.java
-    public Car(){
-        this.make = "Lamborghini";
-        this.model = "Urus";
-        this.year = 2018;
+class Car {
 
-    }
-    public Car(String make, String model, int year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
-=======
-    static {
-        x = 10;
-        y = x * 2;
-        z = calculateZ(x, y);
->>>>>>> c21615ad385eea7496c6007ef32f7b0dad58704a:Exec7.java
-    }
+    String model;
+    int year;
+    String make;
 
-    static int calculateZ(int x, int y)
-    {
-        return x * y + 10;
+    public Car(String make, String model, int year) {
+
+        this.make = (make == null || make.isEmpty()) ? "Unknown Make" : make;
+
+        this.model = (model == null || model.isEmpty()) ? "Unknown Model" : model;
+
+        this.year = (year <= 0) ? 2000 : year;
     }
 }
